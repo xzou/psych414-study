47518c3c3ca49b1615d25f447e54f37feed08a5d
Cindy Zou <xzou@email.wm.edu>
2016-05-04 12:59:51 -0400

    n

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var methodOverride = require('method-override');
 //============================================================
 //mongoose.connect(db.mlab_url || db.url);
 //mongoose.connect(db.url);
-mongoose.connect(db.mlab_url);
+//mongoose.connect(db.mlab_url);
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
